2e9cfb7e3d4ce97862f0f4ce1ecba2654bdc6600
deniz <deniz.hofmeister@gmail.com>
2023-05-26 20:13:49 +0000

    setup of vscode

--- src/rdcli/src/main.rs
+++ src/rdcli/src/main.rs
@@ -5,5 +5,10 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
     
 }
