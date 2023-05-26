4edc83dd72ad6b18328a2e4bcb2a0ba1f4de7800
deniz <deniz.hofmeister@gmail.com>
2023-05-26 20:07:03 +0000

    rust-init

--- src/chown.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Get current user and group
-user=$(id -u -n)
-group=$(id -g -n)
-
-# Change owner of all subdirectories and files in the current directory
-sudo chown -R $user:$group .
--- src/rdcli/src/rdcli/build.py
+++
@@ -1,2 +0,0 @@
-def build(args):
-    print(f"Building package")
